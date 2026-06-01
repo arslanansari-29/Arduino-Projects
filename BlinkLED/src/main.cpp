861c014368eab37b240a2fdd80a4eada59c3a9d3
arslanansari-29 <shamim0951@gmail.com>
2026-06-01 17:27:38 +0530

    git add. git commit -m "Added BlinkLED project in Arduino-Projects repository." git push origin main

---
+++ BlinkLED/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// Pin 13 is the built-in LED on Arduino Uno
+// You can also connect an external LED to pin 13
+#define LED_PIN 13
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // Set pin 13 as output
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH); // Turn LED ON
+  delay(1000);                 // Wait 1 second
+
+  digitalWrite(LED_PIN, LOW);  // Turn LED OFF
+  delay(1000);                 // Wait 1 second
+}
